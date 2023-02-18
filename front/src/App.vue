8b9ef489e54685003a83310a9507676049a1ca55
Loïc Gombeaud <loic.antoinegombeaud@gmail.com>
2023-02-18 18:41:27 +0100

    feat(front): Fully re-write with Vue.js

---
+++ front/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import "leaflet/dist/leaflet.css"
+import {
+  LMap,
+  LTileLayer,
+} from "@vue-leaflet/vue-leaflet"
+import Radars from "./components/Radars.vue"
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    Radars,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: [44.84, -0.57],
+    }
+  },
+}
+</script>
+
+<template>
+  <div style="height:100vh; width:100vw">
+    <l-map ref="map" v-model:zoom="zoom" :center="center">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+      <Radars />
+    </l-map>
+  </div>
+</template>
---
+++ front/src/components/Radars.vue
@@ -0,0 +1,44 @@
+<script>
+import {
+  LMarker,
+  LPopup,
+} from "@vue-leaflet/vue-leaflet"
+import RadarPopup from "./RadarPopup.vue"
+
+export default {
+  data() {
+    return {
+      radars: [],
+      popupOptions: {
+        maxWidth: 10000,
+      },
+    }
+  },
+  components: {
+    LMarker,
+    LPopup,
+    RadarPopup,
+  },
+  methods: {
+    async fetchRadars() {
+      //TODO parametrize?
+      const res = await fetch("http://radars.loicgombeaud.com/radars")
+      this.radars = await res.json()
+    },
+  },
+  mounted() {
+    this.fetchRadars()
+  },
+}
+</script>
+
+<template>
+  <l-marker v-for="radar of radars" :lat-lng="[radar.latitude, radar.longitude]">
+    <l-popup :options="popupOptions">
+      <RadarPopup 
+        :radarId="radar.id"
+        :radarAddress="radar.address"
+      />
+    </l-popup>
+  </l-marker>
+</template>
