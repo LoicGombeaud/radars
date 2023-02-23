a9439349d5325f4bc02570949f353affc92bab2c
Loïc Gombeaud <loic.antoinegombeaud@gmail.com>
2023-02-23 17:03:51 +0100

    feat(front): Get the actual radar photo URLs
    No kitten was harmed in the making of this commit.

--- front/src/components/RadarPhoto.vue
+++ front/src/components/RadarPhoto.vue
@@ -5,8 +5,7 @@ export default {
   },
   methods: {
     getRadarPhotoUrl() {
-      //TODO get actual URL (obviously)
-      return "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+      return new URL(`../assets/img/${this.radarId}.webp`, import.meta.url).href
     },
   },
 }
