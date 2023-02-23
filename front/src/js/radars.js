199b14c1bae91728ce945721094f0722f7d8759d
Loïc Gombeaud <loic.antoinegombeaud@gmail.com>
2023-02-23 14:19:55 +0100

    feat(front): Simplify radar-fetching logic

--- front/src/js/radars.js
+++ front/src/js/radars.js
@@ -3,13 +3,8 @@ import { reactive } from "vue"
 const backUrl = "https://radars.loicgombeaud.com"
 
 export const radars = reactive({
-  all: [],
-  async getAll() {
-    if (!this["all"] || this["all"].length == 0) {
-      const res = await fetch(`${backUrl}/radars`)
-      this["all"] = await res.json()
-    }
-    console.log(this["all"])
-    return this.all
-  },
+  all: await (async () => {
+    const res = await fetch(`${backUrl}/radars`)
+    return await res.json()
+  })(),
 })
