7b5edf9c6859bb4d1542398f3dc58db0af72a6da
Loïc Gombeaud <loic.antoinegombeaud@gmail.com>
2023-02-24 02:13:05 +0100

    feat(back): Add 'latest' routes, to replace 'yesterday'

--- back/back.py
+++ back/back.py
@@ -74,22 +74,28 @@ async def get_hourly_statistics(sensor_id, year, month, day):
     return res
 
 @app.get('/statistics/hourly/{sensor_id}/yesterday')
-async def get_hourly_statistics(sensor_id):
+async def get_hourly_statistics_yesterday(sensor_id):
     queried_datetime_start = date.today() - timedelta(days=1)
-    queried_datetime_end = date.today()
+    return await get_hourly_statistics(sensor_id,
+                                       queried_datetime_start.year,
+                                       queried_datetime_start.month,
+                                       queried_datetime_start.day)
+
+@app.get('/statistics/hourly/{sensor_id}/latest')
+async def get_hourly_statistics_latest(sensor_id):
     pconn = pool.get_connection()
-    cursor = pconn.cursor()
-    cursor.execute('SELECT * FROM statistic_hourly WHERE sensor_id = ? AND datetime >= ? AND datetime < ?',
-                   (sensor_id,
-                    queried_datetime_start.isoformat(),
-                    queried_datetime_end.isoformat()))
-    res = cursor.fetchall()
+    cursor = pconn.cursor(dictionary=True)
+    cursor.execute('SELECT datetime FROM measurement ORDER BY datetime DESC LIMIT 1')
+    latest_datetime = cursor.fetchone()["datetime"]
     cursor.close()
     pconn.close()
-    return res
+    return await get_hourly_statistics(sensor_id,
+                                       latest_datetime.year,
+                                       latest_datetime.month,
+                                       latest_datetime.day)
 
 @app.get('/statistics/daily/{sensor_id}/{year}/{month}/{day}')
-async def get_hourly_statistics(sensor_id, year, month, day):
+async def get_daily_statistics(sensor_id, year, month, day):
     queried_datetime = datetime(int(year),
                                 int(month),
                                 int(day))
@@ -104,14 +110,22 @@ async def get_hourly_statistics(sensor_id, year, month, day):
     return res
 
 @app.get('/statistics/daily/{sensor_id}/yesterday')
-async def get_hourly_statistics(sensor_id):
+async def get_daily_statistics_yesterday(sensor_id):
     queried_datetime = date.today() - timedelta(days=1)
+    return await get_daily_statistics(sensor_id,
+                                      queried_datetime.year,
+                                      queried_datetime.month,
+                                      queried_datetime.day)
+
+@app.get('/statistics/daily/{sensor_id}/latest')
+async def get_daily_statistics_yesterday(sensor_id):
     pconn = pool.get_connection()
-    cursor = pconn.cursor()
-    cursor.execute('SELECT * FROM statistic_daily WHERE sensor_id = ? AND datetime = ?',
-                   (sensor_id,
-                    queried_datetime.isoformat()))
-    res = cursor.fetchone()
+    cursor = pconn.cursor(dictionary=True)
+    cursor.execute('SELECT datetime FROM measurement ORDER BY datetime DESC LIMIT 1')
+    latest_datetime = cursor.fetchone()["datetime"]
     cursor.close()
     pconn.close()
-    return res
+    return await get_daily_statistics(sensor_id,
+                                      latest_datetime.year,
+                                      latest_datetime.month,
+                                      latest_datetime.day)
